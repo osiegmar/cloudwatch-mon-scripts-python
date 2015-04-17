8bf5ce170b02b2350f2fa93a7bcdfd3fe3908828
Al B <al@ACAC0DF1.ipt.aol.com>
2015-04-17 16:35:35 +0100

    Hard-code the requirements since requirements.txt isn't in the tarball from pypi.

--- setup.py
+++ setup.py
@@ -12,22 +12,6 @@ def readme():
             return f.read()
 
 
-def stripped_reqs(fd):
-    return (l.strip() for l in fd)
-
-
-def parse_requirements(requirements):
-    with open(requirements) as f:
-        return [l for l in stripped_reqs(f) if l and not l.startswith("#")]
-
-
-requirements_path = os.path.join(os.path.dirname(os.path.normpath(__file__)),
-                                 'requirements.txt')
-
-
-reqs = parse_requirements(requirements_path)
-
-
 setup(name='cloudwatchmon',
       version=VERSION,
       description='Linux monitoring scripts for CloudWatch',
@@ -39,7 +23,7 @@ setup(name='cloudwatchmon',
       keywords="monitoring cloudwatch amazon web services aws ec2",
       zip_safe=True,
       packages=find_packages(),
-      install_requires=reqs,
+      install_requires=['boto==2.38.0', 'wsgiref==0.1.2'],
       entry_points={'console_scripts': [
           'mon-get-instance-stats.py=cloudwatchmon.cli.get_instance_stats:main',
           'mon-put-instance-stats.py=cloudwatchmon.cli.put_instance_stats:main',
